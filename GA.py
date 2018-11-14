0e77fa8cfba642a99a7874fe60f3d5829a8f557f
lelandwilliams <lelandhwilliams@gmail.com>
2018-11-13 20:40:20 -0600

    added line to set operator_weights

--- GA.py
+++ GA.py
@@ -4,6 +4,7 @@ from collections import ordereddict
 class GA:
   def __init__(self):
     self.operators = []
+    self.operator_weights = dict((op,1) for op in self.operators)
   
   def main_loop(self):
     pass
