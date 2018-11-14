70ec3a28e901aa8fa1cf87d06f9ea329ad56f892
lelandwilliams <lelandhwilliams@gmail.com>
2018-11-13 20:38:23 -0600

    Create GA.py

---
+++ GA.py
@@ -0,0 +1,12 @@
+import random
+from collections import ordereddict
+
+class GA:
+  def __init__(self):
+    self.operators = []
+  
+  def main_loop(self):
+    pass
+  
+  def new_child(self):
+    random.weight
