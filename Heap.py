b745b7ff8a6883c4a9e4ccfe7ce689871b47c11b
Lee Williams <lelandhwilliams@gmail.com>
2018-11-21 09:57:37 -0600

    Added MaxHeap

--- Heap.py
+++ Heap.py
@@ -66,3 +66,56 @@ class MinHeap:
         self.heapify()
         return swap_val
 
+class MaxHeap:
+    def __init__(self, item_val = None, initial_list = None):
+        self.heap = [None]
+        self.size =  0
+        if item_val is None:
+            self.item_val = lambda x: x
+        else:
+            self.item_val = item_val
+        if initial_list is not None:
+            for item in initial_list:
+                self.heap.append(item)
+                self.size += 1
+            for i in range(self.size//2,0,-1):
+                self.heapify(i)
+
+    def heapify(self, i = 1):
+        l = 2 * i
+        r = l + 1
+        largest  = i
+        if l <= self.size and self.item_val(self.heap[l]) > self.item_val(self.heap[i]) :
+            largest = l
+        if r <= self.size and self.item_val(self.heap[r]) > self.item_val(self.heap[largest]) :
+            largest = r
+        if largest != i:
+            item = self.heap[i]
+            self.heap[i] = self.heap[largest]
+            self.heap[largest] = item
+            self.heapify(largest)
+
+    def insert(self, item):
+        self.size += 1
+        self.heap.append(item) 
+        i = self.size
+        while i > 1 and self.item_val(self.heap[i]) > self.item_val(self.heap[i//2]):
+            swap_val = self.heap[i//2]
+            self.heap[i//2] = self.heap[i]
+            self.heap[i] = swap_val
+            i = i//2
+
+    def extract(self):
+        if self.size < 1:
+            raise HeapUnderflowError
+
+        if self.size == 1:
+            self.size = 0
+            return self.heap.pop(-1)
+
+        self.size -= 1
+        swap_val = self.heap[1]
+        self.heap[1] = self.heap.pop(-1)
+        self.heapify()
+        return swap_val
+
