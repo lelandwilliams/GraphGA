39621782e6a8ee874af2ee9ba9263b8fdc91fd9d
Lee Williams <lelandhwilliams@gmail.com>
2018-11-21 10:43:13 -0600

    fleshed out init(), main_loop()

--- GA.py
+++ GA.py
@@ -1,12 +1,27 @@
 import random
 from collections import OrderedDict
+from statistics import mean, stddev
 
 class GA:
-    def __init__(self):
+    def __init__(self, population):
         self.operators = []
         self.operator_weights = dict((op,1) for op in self.operators)
+        self.generation = 0
+        id_function = lambda x : x['fitness']
+        self.heap = MinHeap(id_function)
+        for c in population: 
+            self.heap.insert({'chr': c, 'fitness':self.fitness(c)}
+        self.num_children = self.heap.size() // 4
   
     def main_loop(self):
+        done = False
+        while not done:
+            for in range(self.num_children):
+                c = self.new_child()
+                fitness = self.fitness(c)
+                if self.heap.heap[1]['fitness'] < fitness :
+                    self.heap.extract()
+                    self.heap.insert({'chr': c, 'fitness': fitness})
         pass
   
     def new_child(self):
