5f679e3a82887246c39dd955bfc715d076a94649
Lee Williams <lelandhwilliams@gmail.com>
2018-11-24 13:58:59 -0600

    Initial add:

---
+++ GraphAlgorithms.py
@@ -0,0 +1,106 @@
+from Heap import MinHeap
+import random
+
+# #######################################################
+#       apsp - All Points Shortest Paths
+#
+#  given a graph represented as a matrix with
+#  weighted edges, returns a matrix of 
+#  shortes path lengths between  any pair of vertices
+# #######################################################
+def apsp(W):
+    #
+    # Inner Function Definitions
+    #
+    def newMatrix(size):
+        M = []
+        for _ in range(size):
+            m = [float('inf')] * size
+            M.append(m)
+        return M
+
+    def ExtendShortestPaths(L, a, b):
+        if L[a] is None:
+            L[a] = ExtendShortestPaths(L, a//2, a - a//2)
+        if L[b] is None:
+            L[b] = ExtendShortestPaths(L, b//2, b - b//2)
+        n = len(l[a])
+        M = newMatrix(n)
+        for i in range(n):
+            for j in range(n):
+                for k in range(n):
+                    M[i][j] = min(M[i][j], L[a][i][k] + L[b][k][j])
+        return M
+    
+    #
+    # Main function definition
+    #
+
+    n = len(W) -1 # since longest possible path is number of vertices -1
+    L = [None] * (n+1) # initialzie L, the matrix of Matrix powers
+    L[1] = W
+    
+    # Create L[0]
+    
+    l = newMatrix(n)
+    for i in range(n):
+        l[i][i] = 1
+    L[0] = l
+    
+    L[n] = ExtendShortestPaths(L, n//2, n - n//2)
+    return L
+    
+# ############################################################
+#           prims.py
+#  the prims() function takes as paremeters:
+#
+# D: a dictionary of key value pairs
+# where the keys are labels of vertices, 
+# and the values are dictionaries of labels - distance pairs
+#
+# returns an edge list unless to_graph is set to True
+# ############################################################
+def prims(self, D, start = None, to_graph = False, dist_graph = False): 
+    if start is None:
+        start = random.selection(D.keys())
+
+    visited = [start]
+    edges = []
+    id_function = lambda x : x['weight']
+    heap = MinHeap(item_val = id_function)
+
+    for v in D.keys():
+        if v in visited:
+            continue
+        for dest,dist in D[v].iteritems():
+            if dest in visited:
+                continue
+            heap.insert({'origen':v, 'destination':dest, 'weight': dist})
+        e = None
+        while e is None:
+            e = heap.extract()
+            if e['destination'] in visited:
+                e = None
+        edges.append(e)
+
+    if to_matrix:
+        # initialize M
+        M = {}
+        for k in D.keys():
+            M[k] = {}
+            for l in D.keys():
+                if dist_graph:
+                    M[k][l] = float('inf')
+                else:
+                    M[k][l] = 0
+
+        for e in edges:
+                if dist_graph:
+                    M[e['origen']][e['destination']] = e['weight']
+                else:
+                    M[e['origen']][e['destination']] = 1
+
+        return M
+
+    return edges
+
--- maketable.py
+++ maketable.py
@@ -2,13 +2,14 @@ from math import cos, asin, sqrt
 from collections import OrderedDict
 
 def distance_from_ll(lat1, lon1, lat2, lon2):
+    """ calculates the earth distance in km between the two coordinates """
     p = 0.017453292519943295     #Pi/180
     a = 0.5 - cos((lat2 - lat1) * p)/2 + cos(lat1 * p) * cos(lat2 * p) * (1 - cos((lon2 - lon1) * p)) / 2
     return 12742 * asin(sqrt(a))
 
 def getDistances(cities):
-    # Given a cities dictionary
-    # returns M, a matrix of distances between cities
+    """ returns a weighted matrix of of distances between cities,
+    created from a dictionary of name->(lat, long) pairs """
     c_names = list(cities.keys())
     M = OrderedDict()
     for i in range(len(c_names)):
@@ -25,11 +26,13 @@ def getDistances(cities):
     #M[c_names[-1] = {} # give last city an empty dictionary
     return M
 
-def getCities(fname, n = -2):
-    # since we use n+1 as limit, 
-    # default will be n = -1
+def getCities(fname, n = None):
+    """ returns a dicitonary of name->(lat, long) pairs """
     f = open(fname, "r")
-    lines = f.readlines()[1:n+1]
+    limit = n+1 # the first line is a header
+    if limit is None:
+        limit = -1
+    lines = f.readlines()[1:limit]
     cities = OrderedDict()
     for l in lines:
         items = l.split()
