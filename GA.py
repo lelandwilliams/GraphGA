6b496e78c03087ba39d26f57fbc23974c533b666
Lee Williams <lelandhwilliams@gmail.com>
2018-11-25 10:41:19 -0600

    created random_child(), chr2edgelist(),a nd fleshed out init()

--- GA.py
+++ GA.py
@@ -2,17 +2,29 @@ import random
 import GraphAlgorithms as galg
 from collections import OrderedDict
 from statistics import mean, stdev
+from Heap import MaxHeap, MinHeap
 
 class GA:
-    def __init__(self, population):
+    def __init__(self, M):
         self.operators = []
         self.operator_weights = dict((op,1) for op in self.operators)
         self.generation = 0
+        self.distances = M
+        self.pop_size = 2 * len(self.distances)
+        self.cities = list(self.distances.keys())
+        self.num_children = self.pop_size // 3
+
+        # Generate the initial population
+        population = []
+        while len(population) < self.pop_size:
+            population.append(self.random_child())
+
+        # The population is stored in a min heap to make mu-lambda
+        # replacement faster
         id_function = lambda x : x['fitness']
         self.heap = MinHeap(id_function)
-        for c in population: 
-            self.heap.insert({'chr': c, 'fitness':self.fitness(c)})
-        self.num_children = self.heap.size() // 4
+#        for c in population: 
+#            self.heap.insert({'chr': c, 'fitness':self.fitness(c)})
   
     def main_loop(self):
         done = False
@@ -25,6 +37,33 @@ class GA:
                     self.heap.insert({'chr': c, 'fitness': fitness})
         pass
   
+    def chr2edgelist(self, c):
+        id_function = lambda x:x['deg']
+        min_function = lambda x:x['dist']
+        deg_heap = MaxHeap(id_function)
+        degrees = {}
+        E = []
+
+        for city, deg in zip(self.cities, c):
+            degrees[city] = deg
+            deg_heap.insert({'v':city, 'deg':deg})
+
+        while deg_heap.size > 0:
+            s = deg_heap.extract()
+            dist_heap = MinHeap(min_function)
+            for city, dist in self.distances[s]:
+                dist_heap.insert('dest':city, 'dist':dist)
+            while dist_heap.size > 0 and degrees[s] > 0:
+                t = dist_heap.extract()
+                if (s,t) not in E and (t,s) not in E and degrees[t] > 0:
+                    E.append((s,t))
+                    degrees[s] -= 1
+                    degrees[t] -= 1
+
+        return E
+
+
+
     def new_child(self):
         operator = random.choices(operator_weights.keys(), operator_weights.values())[0]
         child = operator()
@@ -72,4 +111,21 @@ class GA:
             child[gene2] += random.choice([-1, +1])
 
         return child
+
+    def random_child(self):
+        num_cities = len(self.distances)
+        #c = [1] * num_cities
+        tree = galg.randSpanningTree(list(self.distances.keys()))
+        c = list(galg.edge2chr(self.cities, tree).values())
+        most = num_cities - 1
+        least = 0
+        mode = most // 4
+        num_increments = int(random.triangular(most, least, mode)) + 1
+        idxs = random.choices(list(range(num_cities)), k = num_increments)
+        for i in idxs:
+            c[i] += 1
+
+        return c
+
+
 
