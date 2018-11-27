c5f9037b9140a48367466a2acad7bdaee8341b8e
Lee Williams <lelandhwilliams@gmail.com>
2018-11-26 22:28:13 -0600

    cleaned up getDistances and adding round to distance_from_ll

--- maketable.py
+++ maketable.py
@@ -5,23 +5,29 @@ def distance_from_ll(lat1, lon1, lat2, lon2):
     """ calculates the earth distance in km between the two coordinates """
     p = 0.017453292519943295     #Pi/180
     a = 0.5 - cos((lat2 - lat1) * p)/2 + cos(lat1 * p) * cos(lat2 * p) * (1 - cos((lon2 - lon1) * p)) / 2
-    return 12742 * asin(sqrt(a))
+    return round(12742 * asin(sqrt(a)), 2)
 
 def getDistances(cities):
     """ returns a weighted matrix of of distances between cities,
     created from a dictionary of name->(lat, long) pairs """
     c_names = list(cities.keys())
     M = OrderedDict()
-    for i in range(len(c_names)):
-        M[c_names[i]] = {}
-    for i in range(len(c_names) -1 ):
-        lat1, lon1 = cities[c_names[i]]
+    for city1 in c_names:
+        d = OrderedDict()
+        for city2 in c_names:
+            d[city2] = 0
+        M[city1] = d
+
+    idx = 1
+    for city1 in c_names[:-1]:
+        lat1, lon1 = cities[city1]
 #       print(lat1, ',',  lon1)
-        for j in range(i+1, len(c_names)):
-            lat2, lon2 = cities[c_names[j]]
+        for city2 in c_names[idx:]:
+            lat2, lon2 = cities[city2]
             dist = distance_from_ll(lat1, lon1, lat2, lon2)
-            M[c_names[i]][c_names[j]] = round(dist, 2)
-            M[c_names[j]][c_names[i]] = round(dist, 2)
+            M[city1][city2] = round(dist, 2)
+            M[city2][city1] = round(dist, 2)
+        idx += 1
         
     #M[c_names[-1] = {} # give last city an empty dictionary
     return M
